3c832de8bf8fd7145d71c5c7271a164a356c6322
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2023-04-19 21:17:49 +0900

    [level 0] Title: 두 수의 차, Time: 0.02 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120803. 두 수의 차/두 수의 차.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        boolean result = (-50000 <= num1 && 50000 >= num1 && -50000 <= num2 && 50000 >= num2);
+        if (result) {
+            return num1 - num2;
+        }
+        return 0;
+    }
+}
