f1c987cc58b13b4080a457fe1f5510854749bc80
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2023-04-20 14:00:42 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.java
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -1,11 +1,11 @@
 class Solution {
     public int solution(int num1, int num2) {
         boolean result = (0 <= num1 && 10000 >= num1 && 0 <= num2 && 10000 >= num2);
-        if (result) {
-            if (num1 == num2) {
-                return 1;
-            }
+        int answer = 0;
+        
+        if(result) {
+            answer = (num1==num2) ? 1 : -1;
         }
-        return -1;
+        return answer;
     }
 }
