32c64b6738f05508da6142b30b624ff8a884da2a
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2023-05-04 00:07:03 +0900

    [level 0] Title: k의 개수, Time: 27.59 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.java
@@ -3,9 +3,11 @@ class Solution {
         int answer = 0;
 
         for (int m = i; m <= j; m++) {
-            for (String s : String.valueOf(m).split("")) {
-                if (s.equals(String.valueOf(k))) {
-                    answer++;
+            if (String.valueOf(m).contains(String.valueOf(k))) {
+                for (String s : String.valueOf(m).split("")) {
+                    if (s.equals(String.valueOf(k))) {
+                        answer++;
+                    }
                 }
             }
         }
