05c0290b6fe41288529a935ac7d64706cd085e4c
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2023-05-10 20:30:15 +0900

    [level 0] Title: 문자열 밀기, Time: 0.18 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(String A, String B) {
+        int answer = 0;
+        String[] aStr = A.split("");
+        while (!A.equals(B)) {
+            if (answer > A.length()) {
+                return -1;
+            }
+            String temp = aStr[aStr.length - 1];
+
+            for (int i = aStr.length-1; i >= 1; i--) {
+                aStr[i] = aStr[i-1];
+            }
+
+            aStr[0] = temp;
+            A = String.join("",aStr);
+            answer++;
+        }
+        return answer;
+    }
+}
