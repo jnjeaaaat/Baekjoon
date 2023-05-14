04976bff760b8959a1c9ebd263ebf69eb49aa83b
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2023-05-14 21:44:42 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.02 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.java
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.java
@@ -2,31 +2,40 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int num, int total) {
-        List<Integer> numbers = new ArrayList<>();
-        int temp = total;
-        int sum = total-1;
-        if (total == 0) {
-            for (int i = 0; i < num; i++) {
-                numbers.add(-num/2 + i);
-            }
-            return numbers.stream().mapToInt(i -> i).toArray();
-        }
+//         List<Integer> numbers = new ArrayList<>();
+//         int temp = total;
+//         int sum = total-1;
+//         if (total == 0) {
+//             for (int i = 0; i < num; i++) {
+//                 numbers.add(-num/2 + i);
+//             }
+//             return numbers.stream().mapToInt(i -> i).toArray();
+//         }
         
-        while (sum != total) {
-            sum = 0;
-            for (int i = 0; i < num; i++) {
-                sum += temp - i;
-            }
-            if (sum == total) {
-                for (int i = 0; i < num; i++) {
-                    numbers.add(temp-i);
-                }
-            }
-            temp--;
+//         while (sum != total) {
+//             sum = 0;
+//             for (int i = 0; i < num; i++) {
+//                 sum += temp - i;
+//             }
+//             if (sum == total) {
+//                 for (int i = 0; i < num; i++) {
+//                     numbers.add(temp-i);
+//                 }
+//             }
+//             temp--;
+//         }
+
+//         int[] answer = numbers.stream().mapToInt(i -> i).toArray();
+//         Arrays.sort(answer);
+//         return answer;
+        
+        int[] answer = new int[num];
+        int start = (total / num) - (num - 1) / 2;
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = start + i;
         }
 
-        int[] answer = numbers.stream().mapToInt(i -> i).toArray();
-        Arrays.sort(answer);
         return answer;
     }
 }
