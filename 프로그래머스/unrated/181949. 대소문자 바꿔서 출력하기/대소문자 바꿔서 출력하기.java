0435eace75e09d5eb3d93f865d1e3ba8b2af5b86
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2023-05-15 01:47:17 +0900

    [unrated] Title: 대소문자 바꿔서 출력하기, Time: 169.23 ms, Memory: 68.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
+++ 프로그래머스/unrated/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
@@ -2,18 +2,32 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         String a = sc.next();
+//         String[] aStr = a.split("");
+//         String b = "";
+
+//         for (int i = 0; i < aStr.length; i++) {
+//             if (Character.isLowerCase(aStr[i].charAt(0))) {
+//                 b += aStr[i].toUpperCase();
+//             } else if (Character.isUpperCase(aStr[i].charAt(0))) {
+//                 b += aStr[i].toLowerCase();
+//             }
+//         }
+//         System.out.println(b);
+        
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
-        String[] aStr = a.split("");
-        String b = "";
-
-        for (int i = 0; i < aStr.length; i++) {
-            if (Character.isLowerCase(aStr[i].charAt(0))) {
-                b += aStr[i].toUpperCase();
-            } else if (Character.isUpperCase(aStr[i].charAt(0))) {
-                b += aStr[i].toLowerCase();
+        String answer = "";
+        
+        for (Character ch : a.toCharArray()) {
+            if (Character.isUpperCase(ch)) {
+                answer += Character.toLowerCase(ch);
+            } else if (Character.isLowerCase(ch)) {
+                answer += Character.toUpperCase(ch);
             }
         }
-        System.out.println(b);
+        
+        System.out.println(answer);
     }
 }
