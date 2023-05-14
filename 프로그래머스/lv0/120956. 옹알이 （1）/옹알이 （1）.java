2dddfd739515ab55ffd545fa7176ff6184cb0515
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2023-05-14 22:17:00 +0900

    [level 0] Title: 옹알이 (1), Time: 1.63 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -3,19 +3,29 @@ class Solution {
         int answer = 0;
         String[] word = {"aya", "ye", "woo", "ma"};
 
+//         for (String s : babbling) {
+//             boolean check = true;
+//             for (int i = 0; i < word.length; i++) {
+//                 s = s.replaceAll(word[i]," ");
+//             }
+
+//             for (int i = 0; i < s.length(); i++) {
+//                 if (s.charAt(i) != ' ') {
+//                     check = false;
+//                 }
+//             }
+
+//             if (check) {
+//                 answer++;
+//             }
+//         }
         for (String s : babbling) {
-            boolean check = true;
             for (int i = 0; i < word.length; i++) {
-                s = s.replaceAll(word[i]," ");
+                s = s.replaceAll(word[i], "0");
             }
-
-            for (int i = 0; i < s.length(); i++) {
-                if (s.charAt(i) != ' ') {
-                    check = false;
-                }
-            }
-
-            if (check) {
+            s = s.replaceAll("0","");
+            
+            if (s.length() == 0) {
                 answer++;
             }
         }
