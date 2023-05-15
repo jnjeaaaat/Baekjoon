f038e93a4a07330cd8f54fc76d2dac635e294b45
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2023-05-16 03:34:16 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.17 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int left, int right) {
+        int answer = 0;
+        for (int i = left; i <= right; i++) {
+            if (countP(i) % 2 == 0) {
+                answer += i;
+            } else {
+                answer -= i;
+            }
+        }
+        
+        return answer;
+    }
+    
+    public int countP(int num) {
+        int count = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) count++;
+        }
+        
+        return count;
+    }
+}
