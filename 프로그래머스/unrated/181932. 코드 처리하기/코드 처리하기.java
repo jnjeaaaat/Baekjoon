27fd7861b3b7233cb6eae0d69a7ecdf7678004fb
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2023-05-17 01:18:20 +0900

    [unrated] Title: 코드 처리하기, Time: 0.02 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.java
@@ -1,16 +1,32 @@
 class Solution {
     public String solution(String code) {
-        String answer = "";
-        String[] codeList = code.split("");
-        boolean mode = true;
+//         String answer = "";
+//         String[] codeList = code.split("");
+//         boolean mode = true;
         
-        for (int i = 0; i < codeList.length; i++) {
-            if (codeList[i].equals("1")) mode = !mode;
+//         for (int i = 0; i < codeList.length; i++) {
+//             if (codeList[i].equals("1")) mode = !mode;
             
-            if (mode) {
-                if (!codeList[i].equals("1") && i % 2 == 0) answer += codeList[i];
-            } else {
-                if (!codeList[i].equals("1") && i % 2 != 0) answer+= codeList[i];
+//             if (mode) {
+//                 if (!codeList[i].equals("1") && i % 2 == 0) answer += codeList[i];
+//             } else {
+//                 if (!codeList[i].equals("1") && i % 2 != 0) answer+= codeList[i];
+//             }
+//         }
+        
+//         if (answer.length() == 0) {
+//             answer = "EMPTY";
+//         }
+        int mode = 0;
+        String answer = "";
+        
+        for (int i = 0; i < code.length(); i++) {
+            if (code.charAt(i) == '1') {
+                mode = 1 - mode;
+            } else if (mode == 0 && i % 2 == 0) {
+                answer += code.charAt(i);
+            } else if (mode == 1 && i % 2 == 1) {
+                answer += code.charAt(i);
             }
         }
         
