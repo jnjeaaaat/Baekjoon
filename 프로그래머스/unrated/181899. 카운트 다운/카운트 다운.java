b355a97b0b9f2a017744fc4f8f3c7f45c3cbeab2
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2023-05-23 04:12:58 +0900

    [unrated] Title: 카운트 다운, Time: 2.08 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
@@ -1,14 +1,18 @@
+import java.util.stream.*;
+
 class Solution {
     public int[] solution(int start, int end) {
-        int[] answer = new int[start - end + 1];
-        int i = 0;
+//         int[] answer = new int[start - end + 1];
+//         int i = 0;
         
-        while (start >= end) {
-            answer[i] = start;
-            start -= 1;
+//         while (start >= end) {
+//             answer[i] = start;
+//             start -= 1;
             
-            i++;
-        }
-        return answer;
+//             i++;
+//         }
+//         return answer;
+        
+        return IntStream.rangeClosed(-start, -end).map(i -> -i).toArray();
     }
 }
