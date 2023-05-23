a9292945f189cbae8075e93880f772915b978536
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2023-05-24 01:16:17 +0900

    [unrated] Title: n 번째 원소부터, Time: 2.58 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        List<Integer> answer = new ArrayList<>();
+        for (int i = n - 1; i < num_list.length; i++) {
+            answer.add(num_list[i]);
+        }
+        return answer.stream().mapToInt(i -> i).toArray();
+    }
+}
