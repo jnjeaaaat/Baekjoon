5a57b82bf53861e70d6298d755fad098fe4cc884
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2023-05-27 03:02:52 +0900

    [unrated] Title: 배열 만들기 6, Time: 12.53 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.java
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.java
@@ -2,28 +2,37 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr) {
-        List<Integer> answer = new ArrayList<>();
-        answer.add(arr[0]);
-        int i = 1;
+//         List<Integer> answer = new ArrayList<>();
+//         answer.add(arr[0]);
+//         int i = 1;
         
-        while (i < arr.length) {
-            if (answer.size() != 0) {
-                if (answer.get(answer.size() - 1) == arr[i]) {
-                    answer.remove(answer.size() - 1);
-                } else {
-                    answer.add(arr[i]);
-                }
-            } else {
-                answer.add(arr[i]);
-            }
+//         while (i < arr.length) {
+//             if (answer.size() != 0) {
+//                 if (answer.get(answer.size() - 1) == arr[i]) {
+//                     answer.remove(answer.size() - 1);
+//                 } else {
+//                     answer.add(arr[i]);
+//                 }
+//             } else {
+//                 answer.add(arr[i]);
+//             }
             
-            i++;
+//             i++;
+//         }
+        
+//         if (answer.size() == 0) {
+//             answer.add(-1);
+//         }
+        Stack<Integer> answer = new Stack<>();
+        
+        for (int i : arr) {
+            if (!answer.isEmpty() && answer.peek() == i) {
+                answer.pop();
+            } else {
+                answer.push(i);
+            }
         }
         
-        if (answer.size() == 0) {
-            answer.add(-1);
-        }
-        
-        return answer.stream().mapToInt(e -> e).toArray();
+        return answer.isEmpty() ? new int[]{-1} : answer.stream().mapToInt(e -> e).toArray();
     }
 }
