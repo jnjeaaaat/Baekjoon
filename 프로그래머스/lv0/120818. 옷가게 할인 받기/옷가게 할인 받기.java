18f0aa0e0c89eafa1f19c5bded2c20aa632ff7dc
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2023-07-26 18:23:25 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.07 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -1,13 +1,15 @@
 class Solution {
     public int solution(int price) {
         int answer = price;
+        
         if (answer >= 500000) {
-            answer = (int) (price * 0.8);
+            answer = (int) (price * 0.80);
         } else if (answer >= 300000) {
-            answer = (int) (price * 0.9);
+            answer = (int) (price * 0.90);
         } else if (answer >= 100000) {
             answer = (int) (price * 0.95);
         }
+        
         return answer;
     }
 }
