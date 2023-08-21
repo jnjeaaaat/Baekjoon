b42e3d8ae7c46b78175d5ae3571ec78638c83132
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2023-08-22 00:18:56 +0900

    [level 0] Title: 배열 회전시키기, Time: 2.58 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.java
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.java
@@ -1,22 +1,33 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] numbers, String direction) {
         int[] answer = new int[numbers.length];
+        LinkedList<Integer> list = new LinkedList<>();
+        
+        for (int i : numbers) {
+            list.add(i);
+        }
+        
+        System.out.println(list);
         switch (direction) {
             case "right" :
-                int right = numbers[answer.length-1];
-                for (int i = answer.length-1; i >= 1; i--) {
-                    answer[i] = numbers[i-1];
-                }
-                answer[0] = right;
+                // int right = numbers[answer.length-1];
+                // for (int i = answer.length-1; i >= 1; i--) {
+                //     answer[i] = numbers[i-1];
+                // }
+                // answer[0] = right;
+                list.offerFirst(list.pollLast());
                 break;
             case "left" :
-                int left = numbers[0];
-                for (int i = 1; i < answer.length; i++) {
-                    answer[i-1] = numbers[i];
-                }
-                answer[answer.length-1] = left;
+                // int left = numbers[0];
+                // for (int i = 1; i < answer.length; i++) {
+                //     answer[i-1] = numbers[i];
+                // }
+                // answer[answer.length-1] = left;
+                list.offerLast(list.poll());
                 break;
         }
-        return answer;
+        return list.stream().mapToInt(i -> i).toArray();
     }
 }
