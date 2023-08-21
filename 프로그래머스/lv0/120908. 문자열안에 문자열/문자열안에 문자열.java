2a684c7817c4abe5ff1af812b601cdd785655d78
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2023-08-22 00:42:21 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.03 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -2,13 +2,17 @@ class Solution {
     public int solution(String str1, String str2) {
         int answer = 2;
         String temp = "";
-        for (int i = 0; i < str1.length()-(str2.length()-1); i++) {
-            temp = str1.substring(i,str2.length()+i);
-            if (temp.equals(str2)) {
-                answer = 1;
-                return answer;
-            }
+        
+        if (str1.contains(str2)) {
+            answer = 1;
         }
+        // for (int i = 0; i < str1.length()-(str2.length()-1); i++) {
+        //     temp = str1.substring(i,str2.length()+i);
+        //     if (temp.equals(str2)) {
+        //         answer = 1;
+        //         return answer;
+        //     }
+        // }
         return answer;
     }
 }
