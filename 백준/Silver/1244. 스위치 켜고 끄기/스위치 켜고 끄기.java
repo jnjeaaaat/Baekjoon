641e9ce5180434a96847f68f0175cac7cf4b1671
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2024-05-31 13:54:23 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 128 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] switches = new int[n + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i < switches.length; i++) {
+            switches[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int students = Integer.parseInt(br.readLine());
+        int gender = 0;
+        int numOfSwitches = 0;
+        for (int i = 0; i < students; i++) {
+            st = new StringTokenizer(br.readLine());
+            gender = Integer.parseInt(st.nextToken());
+            numOfSwitches = Integer.parseInt(st.nextToken());
+
+            if (gender == 1) {
+                int index = numOfSwitches;
+                while (index < switches.length) {
+                    changeLight(switches, index);
+                    index += numOfSwitches;
+                }
+
+            } else {
+                changeLight(switches, numOfSwitches); // 현재 위치 변경
+
+                int left = numOfSwitches - 1;
+                int right = numOfSwitches + 1;
+                while (left > 0 && right < switches.length) {
+                    if (switches[left] == switches[right]) {
+                        changeLight(switches, left);
+                        changeLight(switches, right);
+                        left--;
+                        right++;
+
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < switches.length; i++) {
+            sb.append(switches[i]).append(" ");
+
+            if (i % 20 == 0) {
+                sb.append("\n");
+            }
+        }
+
+        System.out.println(sb);
+
+    }
+
+    private static void changeLight(int[] arr, int index) {
+        arr[index] = arr[index] == 1 ? 0 : 1;
+    }
+}
