7f9a2be529afc6f4a762fc3914c5631a389d4f5f
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2024-06-20 13:40:03 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 360 ms, Memory: 32736 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
@@ -1,45 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
 
-  public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-    int n = Integer.parseInt(st.nextToken());
-    int k = Integer.parseInt(st.nextToken());
-    int[] nums = new int[n];
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-    st = new StringTokenizer(br.readLine());
-    for (int i = 0; i < nums.length; i++) {
-      nums[i] = Integer.parseInt(st.nextToken());
+        int[] nums = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] countNum = new int[100_001];
+        int left = 0;
+        int right = 0;
+        int max = 0;
+        while (right < n) {
+
+            if (countNum[nums[right]] + 1 <= k) {
+                countNum[nums[right]]++;
+                right++;
+            } else {
+                countNum[nums[left]]--;
+                left++;
+            }
+
+            max = Math.max(max, right - left);
+        }
+
+        System.out.println(max);
+
     }
-
-    int p1 = 0;
-    int p2 = 0;
-    int max = 0;
-
-    Map<Integer, Integer> map = new HashMap<>();
-    while (p2 < n) {
-
-      if (map.getOrDefault(nums[p2], 0) + 1 <= k) {
-        map.put(nums[p2], map.getOrDefault(nums[p2], 0) + 1);
-        max = Math.max(max, p2 - p1 + 1);
-        p2++;
-      } else {
-        map.put(nums[p1], map.get(nums[p1]) - 1);
-        p1++;
-      }
-
-    }
-
-    System.out.println(max);
-  }
-
 }
