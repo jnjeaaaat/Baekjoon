971678535ca4e7601ed95b338c829ed4dbe5b877
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2024-07-29 13:10:25 +0900

    [level 0] Title: 문자열 뒤집기, Time: 13.68 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,9 +1,8 @@
 class Solution {
     public String solution(String my_string) {
         String answer = "";
-        char[] arr = my_string.toCharArray();
-        for (int i = arr.length - 1; i >= 0; i--) {
-            answer += arr[i];
+        for (int i = my_string.length() - 1; i >= 0; i--) {
+            answer += my_string.charAt(i);
         }
         return answer;
     }
