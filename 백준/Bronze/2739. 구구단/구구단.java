e4d901aca4ba1583d82858522379bbf7ca8176bf
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2026-03-11 19:16:39 +0900

    [Bronze V] Title: 구구단, Time: 100 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,14 +1,25 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
         int N = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < 9; i++) {
-            System.out.println(N + " * " + (i+1) + " = " + N*(i+1));
+            sb.append(N)
+                    .append(" * ")
+                    .append(i + 1)
+                    .append(" = ")
+                    .append(N * (i + 1))
+                    .append("\n");
         }
+
+        System.out.println(sb);
 
     }
 }
