5c4f0bca7875087f6e7cf9fbf883ca92daf5a27e
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2026-03-12 01:17:09 +0900

    [Bronze V] Title: N 찍기, Time: 184 ms, Memory: 18308 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -6,10 +6,13 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-        
+
         for (int i = 0; i < n; i++) {
-            System.out.println(i + 1);
+            sb.append(i + 1).append("\n");
         }
+
+        System.out.println(sb);
     }
 }
