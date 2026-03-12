6c5d910d1194104f8b8556afbc79cde9aa211425
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2026-03-13 02:16:50 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -4,10 +4,6 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static char[][] arr;
-    static char BStart = 'B';
-    static char WStart = 'W';
-    static int Bcnt, Wcnt;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,7 +11,7 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        arr = new char[N][M];
+        char[][] arr = new char[N][M];
         for (int i = 0; i < N; i++) {
             arr[i] = br.readLine().toCharArray();
         }
@@ -23,43 +19,23 @@ public class Main {
         int ans = Integer.MAX_VALUE;
         for (int i = 0; i < N - 7; i++) {
             for (int j = 0; j < M - 7; j++) {
-                Bcnt = 0;
-                Wcnt = 0;
+                int Bcnt = 0;
+                int Wcnt = 0;
                 for (int di = i; di < i + 8; di++) {
                     for (int dj = j; dj < j + 8; dj++) {
-                        count(di, dj);
-                        switchColor();
+                        if ((di + dj) % 2 == 0) {
+                            if (arr[di][dj] != 'B') Bcnt++;
+                            if (arr[di][dj] != 'W') Wcnt++;
+                        } else {
+                            if (arr[di][dj] != 'W') Bcnt++;
+                            if (arr[di][dj] != 'B') Wcnt++;
+                        }
                     }
-                    switchColor();
                 }
                 ans = Math.min(Math.min(Bcnt, Wcnt), ans);
             }
         }
 
         System.out.println(ans);
-    }
-
-    static void count(int x, int y) {
-        if (arr[x][y] != BStart) {
-            Bcnt++;
-        }
-
-        if (arr[x][y] != WStart) {
-            Wcnt++;
-        }
-    }
-
-    static void switchColor() {
-        if (BStart == 'B') {
-            BStart = 'W';
-        } else {
-            BStart = 'B';
-        }
-
-        if (WStart == 'B') {
-            WStart = 'W';
-        } else {
-            WStart = 'B';
-        }
     }
 }
