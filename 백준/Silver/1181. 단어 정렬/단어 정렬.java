05df288d36889eabba619cce5950a1203103d45c
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2026-03-16 19:32:47 +0900

    [Silver V] Title: 단어 정렬, Time: 308 ms, Memory: 26328 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            set.add(br.readLine());
+        }
+
+        List<String> list = new ArrayList<>(set);
+        list.sort((s1, s2) -> {
+            if (s1.length() != s2.length()) {
+                return s1.length() - s2.length();
+            }
+            return s1.compareTo(s2);
+        });
+
+        for (String str : list) {
+            sb.append(str).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
