22ac5f2583e30a49ebe7dc35b91fbed7dc19a875
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2026-03-17 19:17:49 +0900

    [Silver II] Title: 통계학, Time: 528 ms, Memory: 49156 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.java
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -6,43 +6,45 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
 
-        List<Integer> list = new ArrayList<>();
-        List<NumCount> numCounts = new ArrayList<>();
-        Map<Integer, Integer> map = new HashMap<>();
+        int[] arr = new int[N];
+        int[] often = new int[8001];
 
         int sum = 0;
         for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(br.readLine());
+            arr[i] = Integer.parseInt(br.readLine());
 
-            sum += num;
-            list.add(num);
-            map.put(num, map.getOrDefault(num, 0) + 1);
+            sum += arr[i];
+            often[arr[i] + 4000]++;
         }
 
-        Collections.sort(list);
+        Arrays.sort(arr);
 
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            numCounts.add(new NumCount(entry.getKey(), entry.getValue()));
+        int max = 0;
+        for (int i : often) {
+            max = Math.max(max, i);
         }
 
-        numCounts.sort((o1, o2) -> {
-            int result = Integer.compare(o2.howOften, o1.howOften);
-            if (result != 0) {
-                return result;
+        int idx = 0;
+        boolean first = false;
+        for (int i = 0; i < often.length; i++) {
+            if (often[i] == max && first) {
+                idx = i - 4000;
+                break;
+            } else if (often[i] == max) {
+                first = true;
+                idx = i - 4000;
             }
+        }
 
-            return Integer.compare(o1.num, o2.num);
-        });
+        sb.append(Math.round((float) sum / N)).append('\n'); // 평균
+        sb.append(arr[N / 2]).append('\n'); // 중앙값
+        sb.append(idx).append('\n'); // 최빈값
+        sb.append(arr[N - 1] - arr[0]); // 범위
 
-        System.out.println(Math.round((float) sum / N)); // 산술평균
-        System.out.println(list.get(N / 2)); // 중앙값
-        System.out.println(numCounts.size() < 2 ? numCounts.get(0).num :
-                numCounts.get(0).howOften != numCounts.get(1).howOften ?
-                        numCounts.get(0).num :
-                        numCounts.get(1).num); // 최빈값
-        System.out.println(list.get(N - 1) - list.get(0)); // 범위
+        System.out.println(sb);
     }
 
     static class NumCount {
