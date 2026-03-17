8f80fba5903cb040bc966920323b854a262e409d
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2026-03-17 20:12:56 +0900

    [Bronze III] Title: 직각삼각형, Time: 100 ms, Memory: 14088 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st;
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            int[] arr = new int[3];
+
+            arr[0] = Integer.parseInt(st.nextToken());
+            arr[1] = Integer.parseInt(st.nextToken());
+            arr[2] = Integer.parseInt(st.nextToken());
+
+            if (arr[0] == 0 && arr[1] == 0 && arr[2] == 0) {
+                break;
+            }
+
+            Arrays.sort(arr);
+            if (isRight(arr)) {
+                sb.append("right").append('\n');
+            } else {
+                sb.append("wrong").append('\n');
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    public static boolean isRight(int[] arr) {
+        return arr[0] + arr[1] >= arr[2] && Math.pow(arr[0], 2) + Math.pow(arr[1], 2) == Math.pow(arr[2], 2);
+    }
+}
+
