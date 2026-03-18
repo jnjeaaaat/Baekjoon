c86f57fb3335ba80f549cd967ae0d69f43e3d127
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2026-03-18 17:55:54 +0900

    [Bronze III] Title: 웰컴 키트, Time: 124 ms, Memory: 15996 KB -BaekjoonHub

---
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[6];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+
+        int count = 0;
+        for (int i : arr) {
+            count += (i + T - 1) / T;
+        }
+
+        System.out.println(count);
+        System.out.println(N / P + " " + N % P);
+    }
+}
