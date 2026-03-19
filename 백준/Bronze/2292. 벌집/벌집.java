5c51ec22da67cef2ce8ea853bfe28e886edf9380
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2026-03-19 15:19:26 +0900

    [Bronze II] Title: 벌집, Time: 104 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -3,24 +3,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        if (n == 1) {
-            System.out.println(1);
-            System.exit(0);
+        int base = 1;
+        int i = 1;
+        while (true) {
+            if (base >= n) {
+                System.out.println(i);
+                break;
+            }
+            base += (6 * i);
+            i++;
         }
-
-        int sum = 1;
-        int count = 1;
-        while (sum < n) {
-            sum += (count * 6);
-            count++;
-        }
-
-        System.out.println(count);
     }
 }
