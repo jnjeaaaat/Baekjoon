cbff0ef70fd638fc2cb4deca146e61b7638f03ad
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2026-03-27 04:11:14 +0900

    [Silver IV] Title: 듣보잡, Time: 424 ms, Memory: 44064 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.kt
+++ 백준/Silver/1764. 듣보잡/듣보잡.kt
@@ -1,31 +1,10 @@
 fun main() {
     val (n, m) = readln().split(" ").map { it.toInt() }
-    val map = mutableMapOf<String, Int>()
+    val a = List(n) { readln() }.toSet()
+    val b = List(m) { readln() }.toSet()
 
-    repeat(n) {
-        val str = readln()
-        map.put(str, map.getOrDefault(str, 0) + 1)
-    }
+    val common = (a intersect b).sorted()
 
-    repeat(m) {
-        val str = readln()
-        map.put(str, map.getOrDefault(str, 0) + 1)
-    }
-
-    val list = mutableListOf<String>()
-    for (entry in map.entries) {
-        if (entry.value > 1) {
-            list.add(entry.key)
-        }
-    }
-
-    list.sort()
-    println(list.size)
-    val sb = buildString {
-        repeat(list.size) { i ->
-            appendLine(list.get(i))
-        }
-    }
-
-    print(sb)
+    println(common.size)
+    print(common.joinToString("\n"))
 }
