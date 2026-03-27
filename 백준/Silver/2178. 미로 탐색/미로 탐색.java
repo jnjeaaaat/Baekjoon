654620c51e1267439e3dc3f4ff7d01db66d514a4
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2026-03-27 19:03:08 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14664 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -7,65 +7,43 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-  static char[][] board;
-  static int[][] count;
-  static int[] di = {0, 0, 1, -1};
-  static int[] dj = {1, -1, 0, 0};
-  static int n, m;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] board = new int[n][m];
+        int[][] count = new int[n][m];
 
-  public static void main(String[] args) throws IOException {
-
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
-
-    n = Integer.parseInt(st.nextToken());
-    m = Integer.parseInt(st.nextToken());
-
-    board = new char[n][m];
-    count = new int[n][m];
-
-    for (int i = 0; i < n; i++) {
-      String s = br.readLine();
-      board[i] = s.toCharArray();
-    }
-
-    bfs();
-
-  }
-
-  private static void bfs() {
-
-    Queue<Node> queue = new LinkedList<>();
-    queue.offer(new Node(0, 0));
-    count[0][0] = 1;
-
-    while (!queue.isEmpty()) {
-      Node cur = queue.poll();
-
-      for (int d = 0; d < 4; d++) {
-        int nx = cur.i + di[d];
-        int ny = cur.j + dj[d];
-
-        if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
-          if (board[nx][ny] == '1' && count[nx][ny] == 0) {
-            queue.offer(new Node(nx, ny));
-            count[nx][ny] = count[cur.i][cur.j] + 1;
-          }
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < m; j++) {
+                board[i][j] = str.charAt(j) - '0';
+            }
         }
-      }
+
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{0, 0});
+        count[0][0] = 1;
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
+                    if (board[nx][ny] == 1 && count[nx][ny] == 0) {
+                        queue.add(new int[]{nx, ny});
+                        count[nx][ny] = count[cur[0]][cur[1]] + 1;
+                    }
+                }
+            }
+        }
+
+        System.out.println(count[n - 1][m - 1]);
     }
-
-    System.out.println(count[n - 1][m - 1]);
-  }
-
-  static class Node {
-    int i;
-    int j;
-
-    Node(int i, int j) {
-      this.i = i;
-      this.j = j;
-    }
-  }
-
 }
