43b52bc2e9048316398592cc3af9e62b959b97b2
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2026-03-31 21:02:42 +0900

    [Silver III] Title: 2×n 타일링, Time: 108 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static final int MOD = 10_007;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[1001];
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for (int i = 3; i < 1001; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
