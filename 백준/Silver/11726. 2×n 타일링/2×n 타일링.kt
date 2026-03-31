9b85892f51f4be7765005582b3e58e4f6651d77f
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2026-03-31 20:58:02 +0900

    [Silver III] Title: 2×n 타일링, Time: 72 ms, Memory: 12356 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.kt
@@ -0,0 +1,14 @@
+const val MOD = 10_007
+
+fun main() {
+    val n = readln().toInt()
+    val dp = IntArray(1001)
+    dp[1] = 1
+    dp[2] = 2
+
+    for (i in 3..1000) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % MOD
+    }
+
+    print(dp[n])
+}
