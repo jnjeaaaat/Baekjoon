67f9a2cc402a11fee002138c9c3f91be96442464
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2026-04-06 02:46:14 +0900

    [Silver II] Title: 최대 힙, Time: 304 ms, Memory: 25540 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.kt
@@ -0,0 +1,26 @@
+import java.util.*
+
+fun main() {
+    val n = readln().toInt()
+    val queue = PriorityQueue<Int>(compareByDescending { it })
+
+    val sb = buildString {
+        repeat(n) {
+            val x = readln().toInt()
+
+            when (x) {
+                0 -> {
+                    if (queue.isNotEmpty()) {
+                        appendLine(queue.poll())
+                    } else {
+                        appendLine(0)
+                    }
+                }
+
+                else -> queue.add(x)
+            }
+        }
+    }
+
+    print(sb)
+}
