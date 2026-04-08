ad7db70d6336c690ddcf734816cdc67878cca2ef
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2026-04-08 21:38:44 +0900

    [Gold V] Title: 적록색약, Time: 160 ms, Memory: 20424 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.kt
+++ 백준/Gold/10026. 적록색약/적록색약.kt
@@ -2,52 +2,48 @@ fun main() {
     val n = readln().toInt()
     val normal = Array(n) { CharArray(n) }
     val weak = Array(n) { CharArray(n) }
-    val dx = intArrayOf(1, -1, 0, 0)
-    val dy = intArrayOf(0, 0, 1, -1)
 
     repeat(n) { i ->
-        normal[i] = readln().toCharArray()
-    }
-
-    for (i in weak.indices) {
-        for (j in weak[i].indices) {
-            val origin = normal[i][j]
-            weak[i][j] = if (origin == 'G') 'R' else origin
+        val input = readln()
+        repeat(n) { j ->
+            normal[i][j] = input[j]
+            weak[i][j] = if (input[j] == 'G') 'R' else input[j]
         }
     }
 
-    fun countArea(board: Array<CharArray>): Int {
-        val visited = Array(n) { BooleanArray(n) }
+    fun countArea(arr: Array<CharArray>): Int {
         var count = 0
+        val visited = Array(n) { BooleanArray(n) }
+        val dx = intArrayOf(0, 0, 1, -1)
+        val dy = intArrayOf(1, -1, 0, 0)
 
-        fun bfs(x: Int, y: Int) {
+        fun bfs(i: Int, j: Int) {
             val queue = ArrayDeque<Pair<Int, Int>>()
-            queue.add(x to y)
-            visited[x][y] = true
+            queue.add(i to j)
 
             while (queue.isNotEmpty()) {
-                val p = queue.removeFirst()
+                val (x, y) = queue.removeFirst()
 
-                for (d in 0 until 4) {
-                    val nx = p.first + dx[d]
-                    val ny = p.second + dy[d]
+                for (d in 0..3) {
+                    val nx = x + dx[d]
+                    val ny = y + dy[d]
 
                     if (nx in 0 until n && ny in 0 until n) {
-                        if (board[nx][ny] == board[x][y]
-                            && !visited[nx][ny]
-                        ) {
+                        if (arr[nx][ny] == arr[i][j] && !visited[nx][ny]) {
                             visited[nx][ny] = true
                             queue.add(nx to ny)
                         }
                     }
                 }
             }
+
         }
 
         for (i in 0 until n) {
             for (j in 0 until n) {
                 if (!visited[i][j]) {
                     count++
+                    visited[i][j]
                     bfs(i, j)
                 }
             }
