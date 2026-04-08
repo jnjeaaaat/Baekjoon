38241b90df3df83f0d43ed1d683b2f482926c9ee
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2026-04-08 21:07:06 +0900

    [Bronze II] Title: Hashing, Time: 80 ms, Memory: 12360 KB, Score: 50 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.kt
+++ 백준/Bronze/15829. Hashing/Hashing.kt
@@ -3,12 +3,12 @@ const val MOD = 1_234_567_891
 fun main() {
     val n = readln().toInt()
     val str = readln()
-    var r = 1
+    var r = 1L
 
-    var sum = 0
+    var sum = 0L
     for (i in 0 until n) {
         val num = str[i] - 'a' + 1
-        sum = (sum + (num * r) % MOD) % MOD
+        sum += (num * r) % MOD
         r = (r * 31) % MOD
     }
 
