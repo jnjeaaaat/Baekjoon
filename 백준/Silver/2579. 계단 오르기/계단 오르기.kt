62a25e494984ab0bac651d6d1cb41fdf8fceef52
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2026-04-09 17:33:12 +0900

    [Silver III] Title: 계단 오르기, Time: 80 ms, Memory: 12364 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.kt
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.kt
@@ -1,20 +1,20 @@
 fun main() {
     val n = readln().toInt()
-    val stairs = IntArray(n + 1)
+    val step = IntArray(n) {
+        readln().toInt()
+    }
+
     val dp = IntArray(n + 1)
+    dp[0] = 0
+    if (n > 0) dp[1] = step[0]
+    if (n > 1) dp[2] = step[0] + step[1]
 
-    for (i in 1..n) {
-        stairs[i] = readln().toInt()
+    if (n > 2) {
+        dp[3] = maxOf(step[0], step[1]) + step[2]
     }
 
-    dp[1] = stairs[1]
-
-    if (n >= 2) {
-        dp[2] = stairs[1] + stairs[2]
-    }
-
-    for (i in 3..n) {
-        dp[i] = maxOf(dp[i - 2], dp[i - 3] + stairs[i - 1]) + stairs[i]
+    for (i in 4..n) {
+        dp[i] = maxOf(dp[i - 2], dp[i - 3] + step[i - 2]) + step[i - 1]
     }
 
     print(dp[n])
