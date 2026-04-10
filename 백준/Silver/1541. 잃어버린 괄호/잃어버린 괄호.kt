0ef0c1fb0987845518f96923ce927e542b6f8d7a
Taejun Park <47658862+jnjeaaaat@users.noreply.github.com>
2026-04-10 16:54:14 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 116 ms, Memory: 21444 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.kt
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.kt
@@ -1,44 +1,13 @@
 fun main() {
-    val ex = readln()
+    val parts = readln().split("-")
 
-    fun sumOf(nums: String): Int {
-        var sum = 0
-        var total = 0
-
-        for (num in nums) {
-            if (num != '+') {
-                sum = (sum * 10) + (num - '0')
-            } else {
-                total += sum
-                sum = 0
-            }
-        }
-
-        return total + sum
+    fun sumOf(part: String): Int {
+        return part.split("+").sumOf() { it.toInt() }
     }
 
-    var answer = 0
-    var end = ex.length
-    var sb = StringBuilder()
-    var flag = false
-    for (i in 0..end - 1) {
-        if (ex[i] != '-') {
-            sb.append(ex[i])
-        } else {
-            if (!flag) {
-                answer += sumOf(sb.toString())
-                flag = true
-            } else {
-                answer -= sumOf(sb.toString())
-            }
-            sb = StringBuilder()
-        }
-    }
-
-    if (!flag) {
-        answer += sumOf(sb.toString())
-    } else {
-        answer -= sumOf(sb.toString())
+    var answer = sumOf(parts[0])
+    for (i in 1 until parts.size) {
+        answer -= sumOf(parts[i])
     }
 
     print(answer)
